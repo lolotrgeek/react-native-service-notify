5450bfd096e3860d2ff0ef2b9613e18b9024d4f5
lolotrgeek <lolotrgeek@gmail.com>
2020-04-29 14:08:54 -0500

    dependencies installed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { setHeartBeat, store } from './service/LocalStore';
 import useCounter from './hooks/useCounterNative'
-import {gun} from './constants/Data'
+import { getProjects, getRunningTimer, getRunningProject, getTimers, updateState } from './constants/Effects'
+import { createProject, createTimer, finishTimer } from './constants/Data';
 
 export default function App() {
   const { status, state, count, setCount, start, stop, startService, stopService } = useCounter()
+  const [online, setOnline] = useState(0)
+  const [projects, setProjects] = useState([])
+  const [timers, setTimers] = useState([])
+  const [current, setCurrent] = useState([])
+  const [runningTimer, setRunningTimer] = useState('')
+  const [runningProject, setRunningProject] = useState('')
+
+  useEffect(() => getProjects({ setProjects }), [online])
+  useEffect(() => getRunningTimer({ setCount, start, stop, setRunningTimer }), [online])
+  useEffect(() => getRunningProject({ setRunningProject, runningTimer }), [runningTimer])
+  useEffect(() => getTimers({ current, timers, setCurrent, setTimers }), [online])
+
   return (
     <View style={styles.container}>
-      <Text style={{color: status === "STARTED" ? 'green' : 'red' }}>{status}</Text>
-      <Text style={{color: state === "PAUSED" ? 'red' : 'green' }}>{state}</Text>
+      <Text style={{ color: status === "STARTED" ? 'green' : 'red' }}>{status}</Text>
+      <Text style={{ color: state === "PAUSED" ? 'red' : 'green' }}>{state}</Text>
+      <Text>{`Running Project: ${runningProject[1] ? runningProject[1].name : ''}`}</Text>
       <Text>{count}</Text>
-      <Button title='Start Service' style={styles.button} onPress={() => {startService()}} />
-      <Button title='Start Counter' style={styles.button} onPress={() => {start()}} />
-      <Button title='Stop Counter' style={styles.button} onPress={() => stop()} />
+      <Button title='Start Service' style={styles.button} onPress={() => { startService() }} />
       <Button title='Stop Service' style={styles.button} onPress={() => stopService()} />
-      <Button title='Clear' style={styles.button} onPress={() => { store.dispatch(setHeartBeat(0)); setCount(0); }} />
-      <Button title='Data' style={styles.button} onPress={() => gun.get('test').put({test: 'dataApp'})} />
+      {/* <Button title='Clear' style={styles.button} onPress={() => { store.dispatch(setHeartBeat(0)); setCount(0); }} /> */}
+      <Button title='New Project' style={styles.button} onPress={() => { createProject('test', '#000') }} />
+      <Button title='New Timer' style={styles.button} onPress={() => {
+        if (projects.length === 0) console.log('No Projects')
+        else {
+          // createTimer(projects[0][0])
+          start()
+        }
+      }} />
+      <Button title='Stop Timer' style={styles.button} onPress={() => {
+        if (runningTimer.length === 0) console.log('No Running Timer')
+        else {
+          finishTimer(runningTimer)
+          stop()
+        }
+      }}
+      />
     </View>
   );
 }
@@ -31,8 +58,8 @@ const styles = StyleSheet.create({
   button: {
     margin: 20,
   },
-  status : {
-    
+  status: {
+
     fontSize: 30,
   }
 });
