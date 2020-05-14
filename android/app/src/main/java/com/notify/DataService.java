077a6438554c71d07c9bfdfd255935a5ea1b54ed
lolotrgeek <lolotrgeek@gmail.com>
2020-05-14 07:59:39 -0500

    complex folder build

--- android/app/src/main/java/com/notify/DataService.java
+++ android/app/src/main/java/com/notify/DataService.java
@@ -4,7 +4,10 @@ import android.app.Service;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.Context;
-import android.os.AsyncTask;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.os.IBinder;
 
 import android.app.Notification;
@@ -15,6 +18,11 @@ import android.os.Build;
 import android.util.Log;
 
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -45,12 +53,21 @@ public class DataService extends Service {
 
             if( !_startedNodeAlready ) {
                 _startedNodeAlready=true;
-                startNodeWithArguments(new String[]{"node", "-e",
-                        "var http = require('http'); " +
-                                "var versions_server = http.createServer( (request, response) => { " +
-                                "  response.end('Versions: ' + JSON.stringify(process.versions)); " +
-                                "}); " +
-                                "versions_server.listen(3000);"
+                //The path where we expect the node project to be at runtime.
+                String nodeDir=getApplicationContext().getFilesDir().getAbsolutePath()+"/nodejs-project";
+                if (wasAPKUpdated()) {
+                    //Recursively delete any existing nodejs-project.
+                    File nodeDirReference=new File(nodeDir);
+                    if (nodeDirReference.exists()) {
+                        deleteFolderRecursively(new File(nodeDir));
+                    }
+                    //Copy the node project from assets into the application's data path.
+                    copyAssetFolder(getApplicationContext().getAssets(), "nodejs-project", nodeDir);
+
+                    saveLastUpdateTime();
+                }
+                startNodeWithArguments(new String[]{"node",
+                        nodeDir+"/main.js"
                 });
             }
         }
@@ -59,7 +76,7 @@ public class DataService extends Service {
         @Override
         public void run() {
             int count = 0;
-            int maxTries = 3;
+            int maxTries = 10;
             String nodeResponse="";
             while(true) {
                 try {
@@ -127,5 +144,102 @@ public class DataService extends Service {
                 .setOnlyAlertOnce(true).setPriority(NotificationCompat.PRIORITY_HIGH).setOngoing(true).build();
         startForeground(SERVICE_NOTIFICATION_ID, notification);
         return START_STICKY;
+    }
+
+    private boolean wasAPKUpdated() {
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences("NODEJS_MOBILE_PREFS", Context.MODE_PRIVATE);
+        long previousLastUpdateTime = prefs.getLong("NODEJS_MOBILE_APK_LastUpdateTime", 0);
+        long lastUpdateTime = 1;
+        try {
+            PackageInfo packageInfo = getApplicationContext().getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0);
+            lastUpdateTime = packageInfo.lastUpdateTime;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return (lastUpdateTime != previousLastUpdateTime);
+    }
+
+    private void saveLastUpdateTime() {
+        long lastUpdateTime = 1;
+        try {
+            PackageInfo packageInfo = getApplicationContext().getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0);
+            lastUpdateTime = packageInfo.lastUpdateTime;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences("NODEJS_MOBILE_PREFS", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putLong("NODEJS_MOBILE_APK_LastUpdateTime", lastUpdateTime);
+        editor.commit();
+    }
+
+    private static boolean deleteFolderRecursively(File file) {
+        try {
+            boolean res=true;
+            for (File childFile : file.listFiles()) {
+                if (childFile.isDirectory()) {
+                    res &= deleteFolderRecursively(childFile);
+                } else {
+                    res &= childFile.delete();
+                }
+            }
+            res &= file.delete();
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean copyAssetFolder(AssetManager assetManager, String fromAssetPath, String toPath) {
+        try {
+            String[] files = assetManager.list(fromAssetPath);
+            boolean res = true;
+
+            if (files.length==0) {
+                //If it's a file, it won't have any assets "inside" it.
+                res &= copyAsset(assetManager,
+                        fromAssetPath,
+                        toPath);
+            } else {
+                new File(toPath).mkdirs();
+                for (String file : files)
+                    res &= copyAssetFolder(assetManager,
+                            fromAssetPath + "/" + file,
+                            toPath + "/" + file);
+            }
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean copyAsset(AssetManager assetManager, String fromAssetPath, String toPath) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = assetManager.open(fromAssetPath);
+            new File(toPath).createNewFile();
+            out = new FileOutputStream(toPath);
+            copyFile(in, out);
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+            return true;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
     }
 }
