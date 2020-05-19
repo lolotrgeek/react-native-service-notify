b89d6a36eaab6c4b5a6c87f711379a515680a6cf
lolotrgeek <lolotrgeek@gmail.com>
2020-05-19 12:25:33 -0500

    testing sql statements, needs further testing

--- android/app/src/main/assets/nodejs-project/data.js
+++ android/app/src/main/assets/nodejs-project/data.js
@@ -4,12 +4,12 @@ const db = new sqlite.Database('testDB.db', 'OPEN_READWRITE | OPEN_CREATE', err 
     console.log('[node] db :' + err)
 } )
 
+const tableName = 'test'
 
+db.run(`CREATE TABLE IF NOT EXISTS ${tableName} (keyField PRIMARY KEY, key, field, val, rel, state, type)`, [], (err) => {
+    console.log(err ? err : 'Insert Success!')
+})
 
-// db.run(`CREATE TABLE IF NOT EXISTS ${this.tableName} (keyField PRIMARY KEY, key, field, val, rel, state, type)`, [], (err) => {
-//     console.log(err ? err : 'Insert Success!')
-// })
-
-// db.all(`SELECT * FROM ${this.tableName} WHERE keyField = ?`, [], (err, rows) => {
-//     err ? console.log(err) : rows.map(row=> console.log('Row: ', row))
-// })
+db.all(`SELECT * FROM ${tableName} WHERE keyField = ?`, [], (err, rows) => {
+    err ? console.log(err) : rows.map(row=> console.log('Row: ', row))
+})
